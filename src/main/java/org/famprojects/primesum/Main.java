bd117cbc436ef322c2e74121a55056328e5b4e2f
TonyYuta <tonykagaya@gmail.com>
2018-02-04 12:19:30 -0800

    done all methods wo TCs

--- src/main/java/org/famprojects/primesum/Main.java
+++ src/main/java/org/famprojects/primesum/Main.java
@@ -15,22 +15,11 @@ public class Main {
 	public static String inpString = "";
 	public static BigInteger[] bidata = new BigInteger[10]; // Big Integer data
 	public static BigInteger[] am = new BigInteger[45]; // Arithmetic Means
-	public static BigInteger[] ams = new BigInteger[45]; // Arithmetic Means sum digits
-	public static int[] primeNumbers = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109,	 113, 127, 131,	137, 139, 149, 151, 157, 163, 167, 173, 179, 181};
-
 	
 	public static void main(String... args) throws Throwable {
 		dataBI();
-		allArithmeticMeans();
-        System.out.println("1===================================");
-		printOutArray(bidata);
-        System.out.println("2===================================");
-        printOutArray(am);		
-        allArithmeticMeansSum(am);
-        System.out.println("3===================================");
-        printOutArray(ams);		
-		
-    }
+		allArithmeticMeansSum();
+      }
        
     public static void verifyInputLength(String inp) {
     	   boolean result;
@@ -74,15 +63,20 @@ public class Main {
  	   return am;
     }
     
-    public static void allArithmeticMeans() {
+    public static void allArithmeticMeansSum() {
     	   int n = 0;
+    	   boolean found = false;;
     	   for(int i = 0; i < 9; i++) {
     		   for(int j = i+1; j < 10; j++) {
     			   am[n] = arithmeticMean(bidata[i], bidata[j]);
-    			   System.out.println("bidata[" + i + "] : bidata[" + j + "] : am[" + n + "] | " +  bidata[i] + ":" + bidata[j] + ":" + am[n]);
+    			   if(isPrimeNumber(sumDigits(am[n]))) {
+    				   found = true;
+    				   System.out.println( (i+1) + ":" + (j+1) + ":" + sumDigits(am[n]));
+    			   }    			   
     			   n++;
     		   }
     	   }
+    	   if(!found) System.out.println("not found");
     }
     
     public static void printOutArray(BigInteger[] arr) {
@@ -91,19 +85,32 @@ public class Main {
     	   }
     }
     
-    public static void allArithmeticMeansSum(BigInteger[] arr) {
-    		for(int i = 0; i < arr.length; i++) {
-    			String digits = arr[i].toString();
-    			int sum = 0;
+    public static int sumDigits(BigInteger num) {
+    	int result = 0;
+		int sum = 0;
+		String digits = num.toString();
 
-	    		for(int j = 0; j < digits.length(); j++) {
-	    			int digit = digits.charAt(j) - '0';
-	    			sum = sum + digit;
-	    		}
-    		ams[i] = BigInteger.valueOf(sum);
-    		//System.out.println(sum);
-    		}     
-    }
-  
-
+    		for(int i = 0; i < digits.length(); i++) {
+    			int digit = digits.charAt(i) - '0';
+    			sum = sum + digit;
+    		}
+		result = sum;
+		return result;
+	}     
+    
+    /**
+     * checks whether an int is prime or not.
+     * @param num
+     * @return boolean
+     * 
+     */
+    public static boolean isPrimeNumber(int num) {
+    		if(num == 1) return false;
+    	    if(num % 2 == 0) return false;
+    	    for(int i = 3; i*i <= num; i += 2) {
+    	        if(num % i == 0)
+    	            return false;
+    	    }
+    	    return true;
+    	}
 }
